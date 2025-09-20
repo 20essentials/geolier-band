bca97de6b776e615faf8ea66b30c50ef5ac763ce
DominicDev <s0l0estudi4r@gmail.com>
2025-09-20 23:21:22 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'GEOLIER';
+export let nameSongs = [
+	"I P’ ME, TU P’ TE", /*1*/
+	"IL MALE CHE MI FAI", /*2*/
+	"2 SECONDI", /*3*/
+	"COME VUOI", /*4*/
+	"HERMES", /*5*/
+	"SO FLY", /*6*/
+	"ME VULEV FA RUOSS", /*7*/
+	"CHIAGNE", /*8*/
+	"L'ULTIMA CANZONE", /*9*/
+	"SOLDATI", /*10*/
+	"NARCOS", /*11*/
+	"COMO TE", /*12*/
+	"QUEEN", /*13*/
+	"NA CATENA", /*14*/
+	"SENZ E ME", /*15*/
+	"VOGL SUL A TE", /*16*/
+	"VITTORIA", /*17*/
+	"CAPO", /*18*/
+	"MONCLER", /*19*/
+	"SCUMPAR", /*20*/
+]
